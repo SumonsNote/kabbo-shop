89714ea428599d1dc3a95da08d2440c9db4a76a2
soab42 <syfuddhin@gmail.com>
2024-11-08 13:13:21 +0600

    fix brand post api

--- app/(dashboard)/dashboard/components/ProductItem.js
+++ app/(dashboard)/dashboard/components/ProductItem.js
@@ -10,7 +10,7 @@ const ProductItem = ({ product }) => {
       <td className="px-6 py-4 whitespace-nowrap">
         <span
           className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-            product.status === "In Stock"
+            product.status == "in stock"
               ? "bg-green-100 text-green-800"
               : product.status === "Low Stock"
               ? "bg-yellow-100 text-yellow-800"
