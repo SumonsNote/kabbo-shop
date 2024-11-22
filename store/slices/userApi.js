df87886b762ebcaf9d89059d29250800a2302f18
MD SUMON H. SHOHAN <sohankhance@gmail.com>
2024-11-22 22:15:27 +0600

    users api and form and trending best-seller offer new-arrival

--- store/slices/userApi.js
+++ store/slices/userApi.js
@@ -3,34 +3,34 @@ import { apiSlice } from "../api/apiSlice";
 export const usersApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     fetchUsers: builder.query({
-      query: () => "/users",
-      providesTags: ["Users"],
-      forceRefetch: true,
-      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        const users = await queryFulfilled;
-        // Handle the fetched data if needed
-      },
+      query: () => "/user",
+      providesTags: ["users"],
+      // forceRefetch: true,
+      // async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+      //   const users = await queryFulfilled;
+      //   // Handle the fetched data if needed
+      // },
     }),
-    fetchUserById: builder.query({
-      query: (id) => `/users/${id}`,
-    }),
-    fetchUserByArea: builder.query({
-      query: (area) => `/users/area/${area}`,
-    }),
+    // fetchUserById: builder.query({
+    //   query: (id) => `/users/${id}`,
+    // }),
+    // fetchUserByArea: builder.query({
+    //   query: (area) => `/users/area/${area}`,
+    // }),
     addUser: builder.mutation({
       query: (user) => ({
-        url: "/users",
+        url: "/user",
         method: "POST",
         body: user,
       }),
-      invalidatesTags: ["Users"],
+      invalidatesTags: ["users"],
     }),
   }),
 });
 
 export const {
-  useFetchUserByIdQuery,
   useFetchUsersQuery,
+  useFetchUserByIdQuery,
   useFetchUserByAreaQuery,
   useAddUserMutation,
 } = usersApi;
